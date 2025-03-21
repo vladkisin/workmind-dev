31174290aca02296ae57d0dd43020c938e5579ab
Uladzislau Kisin <vladkisin1996@gmail.com>
2025-03-21 18:00:41 +0200

    add chunking func

--- data/utils.py
+++ data/utils.py
@@ -25,3 +25,54 @@ def preprocess_and_split_gd(df: pd.DataFrame):
     val_df, test_df = train_test_split(temp_df, test_size=1 / 3, random_state=42)
 
     return train_df, val_df, test_df
+
+
+def chunk_text_by_sentences(sentences, min_chunk=500, max_chunk=1900, target_overlap=160):
+    chunks = []
+    current_chunk = []
+    current_length = 0
+
+    i = 0
+    while i < len(sentences):
+        sentence = sentences[i]
+        sentence_length = len(sentence)
+
+        if sentence_length >= max_chunk:
+            if current_chunk:
+                chunks.append(" ".join(current_chunk))
+                current_chunk = []
+                current_length = 0
+            chunks.append(sentence)
+            i += 1
+            continue
+
+        if current_length + sentence_length + (1 if current_chunk else 0) > max_chunk:
+            if current_length < min_chunk:
+                current_chunk.append(sentence)
+                current_length += sentence_length + (1 if current_chunk[:-1] else 0)
+                i += 1
+                continue
+            else:
+                chunk_text = " ".join(current_chunk)
+                chunks.append(chunk_text)
+                overlap_chunk = []
+                overlap_length = 0
+                j = len(current_chunk) - 1
+                while j >= 0 and (overlap_length < target_overlap or not overlap_chunk):
+                    overlap_chunk.insert(0, current_chunk[j])
+                    overlap_length += len(current_chunk[j]) + 1
+                    j -= 1
+
+                current_chunk = overlap_chunk.copy()
+                current_length = sum(len(s) for s in current_chunk) + (len(current_chunk)-1)
+                continue
+        else:
+            if current_chunk:
+                current_length += 1
+            current_chunk.append(sentence)
+            current_length += sentence_length
+            i += 1
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
+
+    return chunks
